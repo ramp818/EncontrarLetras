5af98093edf77243d02c48b557468e8393cf51d5
Ruben Pacheco <pachecaso9436@gmail.com>
2014-02-23 21:58:10 -0600

    elimino palabra2
    se elimino palabra 2

--- encontrarLetras/main.cpp
+++ encontrarLetras/main.cpp
@@ -5,12 +5,11 @@ using namespace std;
 int main()
 {
     int veces;
-    string palabra1,palabra2;
+    string palabra1;
     cin>>veces;
     while(veces>0)
     {
         cin>>palabra1;
-        cin>>palabra2;
         veces--;
     }
     return 0;

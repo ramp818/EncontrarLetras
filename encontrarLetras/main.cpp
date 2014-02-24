103da9f471d0755148507f3dbe8a562e3e369dc0
Ruben Pacheco <pachecaso9436@gmail.com>
2014-02-23 21:59:04 -0600

    Revert "elimino palabra2"
    This reverts commit 5af98093edf77243d02c48b557468e8393cf51d5.

--- encontrarLetras/main.cpp
+++ encontrarLetras/main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main()
 {
     int veces;
-    string palabra1;
+    string palabra1,palabra2;
     cin>>veces;
     while(veces>0)
     {
         cin>>palabra1;
+        cin>>palabra2;
         veces--;
     }
     return 0;

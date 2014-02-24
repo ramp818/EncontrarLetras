9af66be3005f550fa553ca175584284086eb2662
Ruben Pacheco <pachecaso9436@gmail.com>
2014-02-23 21:52:29 -0600

    Leer palabras
    se empezaron a leer las palabras

---
+++ encontrarLetras/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int veces;
+    string palabra1,palabra2;
+    cin>>veces;
+    while(veces>0)
+    {
+        cin>>palabra1;
+        cin>>palabra2;
+        veces--;
+    }
+    return 0;
+}

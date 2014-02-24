8ecfefc80653ea023b5acbb30131494e08d35e7b
Ruben Pacheco <pachecaso9436@gmail.com>
2014-02-24 00:23:47 -0600

    Encuentra letras
    encuentras letras

--- encontrarLetras/main.cpp
+++ encontrarLetras/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+void funcion(string palabra1,string palabra2)
+{
+    string palabra3;
+    int encuentra,encuentra2=1;
+    for(int i=0;i<palabra1.length();i++)
+    {
+        encuentra=palabra2.find(palabra1[i]);
+        encuentra2=palabra3.find(palabra1[i]);
+        if(encuentra!=-1 && encuentra2 ==-1)
+        {
+            palabra3+=palabra1[i];
+        }
+    }
+    cout<<palabra3;
+}
 
 int main()
 {
@@ -11,6 +28,7 @@ int main()
     {
         cin>>palabra1;
         cin>>palabra2;
+        funcion(palabra1,palabra2);
         veces--;
     }
     return 0;

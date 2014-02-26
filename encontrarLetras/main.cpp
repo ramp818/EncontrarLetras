7dd01390c9c3e7d51c7f2b732c119c706137e685
Ruben Pacheco <pachecaso9436@gmail.com>
2014-02-25 21:32:55 -0600

    Version final
    Orden (n^2)

--- encontrarLetras/main.cpp
+++ encontrarLetras/main.cpp
@@ -3,17 +3,29 @@
 
 using namespace std;
 
+
 void funcion(string palabra1,string palabra2)
 {
     string palabra3;
-    int encuentra,encuentra2=1;
+    int encuentra,cantidad=0;
     for(int i=0;i<palabra1.length();i++)
     {
         encuentra=palabra2.find(palabra1[i]);
-        encuentra2=palabra3.find(palabra1[i]);
-        if(encuentra!=-1 && encuentra2 ==-1)
+        if(encuentra!=-1)
         {
+            palabra2.erase(encuentra,1);
             palabra3+=palabra1[i];
+            char aux;
+            for(int j=0;j<cantidad;j++)
+            {
+                if(palabra3[j]>palabra3[cantidad])
+                {
+                    aux=palabra3[j];
+                    palabra3[j]=palabra3[cantidad];
+                    palabra3[cantidad]=aux;
+                }
+            }
+        ++cantidad;
         }
     }
     cout<<palabra3<<endl;
